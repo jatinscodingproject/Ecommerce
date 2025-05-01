5d9b0f3eccdc09684c189674fd870aaff989f102
Jatin Dixit <152329190+jatinscodingproject@users.noreply.github.com>
2025-05-01 21:07:00 +0530

    code update

--- backend/src/controllers/controller.product.js
+++ backend/src/controllers/controller.product.js
@@ -3,12 +3,12 @@ const { HttpStatus } = require('../utils/utils.httpStatus')
 
 const productController = {
     async addProduct(req, res) {
-        const result = await productServices.addProduct(req, res);
+        const result = await services.addProduct(req, res);
         res.status(result.result === 'pass' ? 201 : 400).json(result);
     },
 
     async getProduct(req, res) {
-        const result = await productServices.getProducts(req, res);
+        const result = await services.getProducts(req, res);
         res.status(result.result === 'pass' ? 200 : 400).json(result);
     },
 
